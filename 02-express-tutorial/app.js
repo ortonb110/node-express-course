8fde6e9370f535b35088f403a827cbb9d26ba539
Orton Bright <ortonb110@gmail.com>
2023-04-08 17:20:01 +0100

    Static Assets

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,18 @@
-const express = require('express');
-
+const express =require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
+
+app.all('*', (req, res)=> {
+    res.status(404).send('<h1>Resource not found!</h1>')
+})
 
 
-
-app.listen(5000, () => {
-    console.log("App started using Express.js");
+app.listen(5000, ()=> {
+    console.log("Server started!")
 })
