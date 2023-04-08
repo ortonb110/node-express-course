b9f9783c710a2ba78863662139164488f632b697
Orton Bright <ortonb110@gmail.com>
2023-04-08 20:38:32 +0100

    Express Params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,18 +1,28 @@
-const express =require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
 
-app.use(express.static('./public'))
+const {products} = require('./data')
 
 app.get('/', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+    res.send("<h1>Welcome to the homepage</h1><a href='/api/products'>Products</a>")
 })
 
-app.all('*', (req, res)=> {
-    res.status(404).send('<h1>Resource not found!</h1>')
-})
+app.get("/api/products", (req, res) => {
+    const newProduct = products.map(items=> {
+        const {id, name, image} = items;
+        return {id, name, image};
+    })
+  res.json(newProduct);
+});
+app.get("/api/products/:productID", (req, res) => {
+  const {productID} = req.params;
+  const singleProduct = products.find((product)=> product.id === Number(productID))
+  if(!singleProduct) {
+    return res.status(404).send("<h1>Product does not exist</h1>")
+  }
+  res.json(singleProduct);
+});
 
-
-app.listen(5000, ()=> {
-    console.log("Server started!")
-})
+app.listen(5000, () => {
+  console.log("Server started on port 5000...");
+});
