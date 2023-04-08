e514159c29981e297cda83915cb229ae8b265a1e
Orton Bright <ortonb110@gmail.com>
2023-04-08 21:11:52 +0100

    Express Query String

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -23,6 +23,21 @@ app.get("/api/products/:productID", (req, res) => {
   res.json(singleProduct);
 });
 
+app.get('/api/v1/query', (req, res)=> {
+  let sortedProducts = [...products];
+  const {search, limit} = req.query;
+  if(search){
+    sortedProducts = sortedProducts.filter((products)=> {
+        return products.name.startsWith(search);
+    })
+  }
+  if(limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  res.status(200).json(sortedProducts);
+})
+
 app.listen(5000, () => {
   console.log("Server started on port 5000...");
 });
