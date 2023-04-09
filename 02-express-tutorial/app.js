df4835f26d61b7d45062d839a930495fa622822e
Orton Bright <ortonb110@gmail.com>
2023-04-09 18:20:52 +0100

    Basic Server setup

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,42 +1,59 @@
 const express = require("express");
 const app = express();
+let { people } = require("./data");
+//Static assets
+app.use(express.static("./methods-public"));
 
-const {products} = require('./data')
-
-app.get('/', (req, res)=> {
-    res.send("<h1>Welcome to the homepage</h1><a href='/api/products'>Products</a>")
-})
-
-app.get("/api/products", (req, res) => {
-    const newProduct = products.map(items=> {
-        const {id, name, image} = items;
-        return {id, name, image};
-    })
-  res.json(newProduct);
-});
-app.get("/api/products/:productID", (req, res) => {
-  const {productID} = req.params;
-  const singleProduct = products.find((product)=> product.id === Number(productID))
-  if(!singleProduct) {
-    return res.status(404).send("<h1>Product does not exist</h1>")
-  }
-  res.json(singleProduct);
+//Api people
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get('/api/v1/query', (req, res)=> {
-  let sortedProducts = [...products];
-  const {search, limit} = req.query;
-  if(search){
-    sortedProducts = sortedProducts.filter((products)=> {
-        return products.name.startsWith(search);
-    })
-  }
-  if(limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
+//Parse form data
+app.use(express.urlencoded({ extended: false }));
+//Parse form JSON
+app.use(express.json());
 
-  res.status(200).json(sortedProducts);
-})
+app.post("/login", (req, res) => {
+  const { name } = req.body.name;
+  if (!name) {
+    return res.send("You did not enter any name!");
+  } else {
+    res.send(`Welcome ${name}`);
+  }
+});
+
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.send("You did not enter any name!");
+  } else {
+    res.status(201).json({ success: true, data: [...people, name] });
+  }
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  } else {
+    res.status(201).json({ success: true, person: name });
+  }
+});
+
+//Update data
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+   const person = people.find(person => person.id === Number(id));
+   if(!person) {
+    return res.status(404).json({success: false, msg:`No person with an id ${id} exist`})
+   } else {
+     res.status(200).json({success:true, msg: "Name update!"});
+   }
+});
 
 app.listen(5000, () => {
   console.log("Server started on port 5000...");
